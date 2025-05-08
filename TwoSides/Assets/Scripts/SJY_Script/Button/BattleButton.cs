573c8ecb7c01928bddb83ba6daf4a5960b8c10c5
jseol0 <79302243+jseol0@users.noreply.github.com>
2025-05-09 01:10:04 +0900

    BattleStage_count

--- TwoSides/Assets/Scripts/SJY_Script/Button/BattleButton.cs
+++ TwoSides/Assets/Scripts/SJY_Script/Button/BattleButton.cs
@@ -22,14 +22,18 @@ public class BattleButton : MonoBehaviour
         //}
         //isUsed[rand] = true;
 
-        int rand = 0;
-        while (isUsed[rand])
-        {
-            rand++;
-        }
-        isUsed[rand] = true;
+        //int rand = 0;
+        //while (isUsed[rand])
+        //{
+        //    rand++;
+        //}
+        //isUsed[rand] = true;
 
-        sceneName += rand.ToString();
+
+        sceneName += Map.Instance.battleNum.ToString();
+        if (Map.Instance.battleNum < 9)
+            Map.Instance.battleNum++;
+
         GameManager.Instance.isClear = false;
         Mapbutton.Instance.activeButton = false;
         Map.Instance.doorConnected = false;
