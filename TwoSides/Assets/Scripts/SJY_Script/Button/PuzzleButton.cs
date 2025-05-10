d8613c55d1fa8371adca41252b79d7a6bfa47316
jseol0 <79302243+jseol0@users.noreply.github.com>
2025-05-10 20:31:18 +0900

    Mapbutton

--- TwoSides/Assets/Scripts/SJY_Script/Button/PuzzleButton.cs
+++ TwoSides/Assets/Scripts/SJY_Script/Button/PuzzleButton.cs
@@ -1,8 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class PuzzleButton : MonoBehaviour
 {
     public GameObject map;
+
+    public string sceneName;
+    [Header("사운드")]
+    public AudioSource audioSource;   // 인스펙터에서 연결
+    public AudioClip clickSound;      // 클릭 시 재생할 사운드
+
     private bool[] isUsed = new bool[3];
     private void Awake()
     {
@@ -11,7 +18,7 @@ public class PuzzleButton : MonoBehaviour
     }
     public void Onclick()
     {
-        string sceneName = "Puzzle";
+        sceneName = "Puzzle";
 
         int rand = Random.Range(0, 3);
         while (isUsed[rand])
@@ -23,12 +30,26 @@ public class PuzzleButton : MonoBehaviour
         rand = 0;
 
         sceneName += rand.ToString();
-        map.SetActive(false);
 
         GameManager.Instance.isClear = false;
         Mapbutton.Instance.activeButton = false;
         Map.Instance.doorConnected = false;
 
+        if (audioSource != null && clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
+        else
+        {
+            Debug.LogError("비상사태 불러박자박사 (AudioSource or Clip 없음)");
+        }
+        StartCoroutine(PlayerAndLoad());
+    }
+
+    private IEnumerator PlayerAndLoad()
+    {
+        yield return new WaitForSeconds(clickSound.length);
+        map.SetActive(false); //로딩이끝났을때로 걸면 가능할듯
         LoadingSceneController.Instance.LoadScene(sceneName);
     }
 }
